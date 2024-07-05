255ba8041e1890bcb7284ce86de6bbcaa336b439
VithorCarlos <vithor.carlos99@hotmail.com>
2024-07-05 20:52:42 -0300

    feat: start project

---
+++ src/env/index.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const schema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "staging"])
+    .default("development"),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
+});
+
+const _env = schema.safeParse(process.env);
+
+if (_env.error) {
+  console.error(
+    "❌ Invalid environment variables",
+    _env.error.formErrors.fieldErrors
+  );
+  throw new Error("❌ Invalid environment variables");
+}
+
+export const env = _env.data;
